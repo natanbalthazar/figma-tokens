543ea95d2ece9ed0d7dd00f197d9a1eabfdb596e
natanbalthazar <natanbalthazar@users.noreply.github.com>
2023-07-21 20:20:22 +0000

    style: Update design tokens

--- build/js/tokens.js
+++ build/js/tokens.js
@@ -1,13 +1,17 @@
 /**
  * Do not edit directly
- * Generated on Fri, 21 Jul 2023 15:22:28 GMT
+ * Generated on Fri, 21 Jul 2023 20:20:22 GMT
  */
 
 module.exports = {
+  "Sizes2": 8,
+  "Sizes4": 16,
+  "Sizes8": 32,
   "ColorWhite": "#ffffff",
   "ColorGray200": "#c1c1c4",
   "ColorGray500": "#50505c",
   "ColorGray800": "#1c1c1f",
   "ColorGray900": "#121214",
+  "ColorPurple400": "#9f82e1",
   "ColorPurple500": "#8257e6"
 };
